818af14d4bb2a69a69aa4f58b45c897af146a975
npallavi2604 <npallavi2604@gmail.com>
2020-05-16 09:15:16 -0700

    first commit

---
+++ src/main/java/com/rns/sudoku/impl/Graph.java
@@ -0,0 +1,10 @@
+package com.rns.sudoku.impl;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class Graph {
+	Node[] vertex = new Node[81];
+	HashMap<Node, List<Node>> edges = new HashMap<Node, List<Node>>();
+	
+}
---
+++ src/main/java/com/rns/sudoku/impl/Node.java
@@ -0,0 +1,5 @@
+package com.rns.sudoku.impl;
+
+public class Node {
+
+}
